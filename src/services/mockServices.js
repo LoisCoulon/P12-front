ab233053150f0a0c7e11b5ad6455395dd4e8c580
Loïs <flojuve@hotmail.fr>
2022-05-19 14:40:39 +0200

    jsdoc update

--- src/services/mockServices.js
+++ src/services/mockServices.js
@@ -1,6 +1,6 @@
 /**
- * Those functions allow the connection to the mocked datas
- * @returns mocked data.json
+ * Get the user informations (mocked)
+ * @returns object
  */
 export function getList() {
   const baseUrl = "../mocked/mainData.json";
@@ -8,18 +8,30 @@ export function getList() {
   return fetch(baseUrl).then((data) => data.json());
 }
 
+/**
+ * Get the user activities (mocked)
+ * @returns object
+ */
 export function getActivity() {
   const baseUrl = "../mocked/activity.json";
 
   return fetch(baseUrl).then((data) => data.json());
 }
 
+/**
+ * Get the user average sessions (mocked)
+ * @returns object
+ */
 export function getDuration() {
   const baseUrl = "../mocked/averageSession.json";
 
   return fetch(baseUrl).then((data) => data.json());
 }
 
+/**
+ * Get the user performances of (mocked)
+ * @returns object
+ */
 export function getRadar() {
   const baseUrl = "../mocked/performance.json";
 
